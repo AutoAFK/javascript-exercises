6a05a67612b862a1d2570cc56b91452cbf3249f6
AutoAFK <ranhaim85@gmail.com>
2026-03-18 20:20:53 +0200

    Loops and Arrays

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...excludeValues) {
+    return array.filter(value => !excludeValues.includes(value));
 };
 
 // Do not edit below this line
--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const isDividedByFour = year % 4 === 0;
+    const isCentury = year % 100 === 0;
+    const isYearDividedByFourHundred = year % 400 === 0;
 
+    if (isDividedByFour && (!isCentury || isYearDividedByFourHundred)){
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
